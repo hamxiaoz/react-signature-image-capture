6891ea2b3c1a05e62e57e7eaeca03cf3012f4fb0
Andrew Zheng <>
2018-09-05 16:31:45 -0700

    sig component

--- src/App.js
+++ src/App.js
@@ -1,15 +1,12 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { Signature } from './components/signature';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome!</h1>
-        </header>
+      <div className="form-container">
+        <Signature></Signature>
       </div>
     );
   }
---
+++ src/components/signature/signature.jsx
@@ -0,0 +1,46 @@
+import React, { Component } from 'react';
+import SignatureCanvas from 'react-signature-canvas';
+import './signature.css';
+
+class Signature extends Component {
+  sigPad = null;
+  constructor(props) {
+    super(props);
+    this.state = {
+      clearButtonDisabled: true
+    };
+  }
+
+  clear = () => {
+    this.sigPad.clear();
+    this.updateClearButton();
+  };
+
+  updateClearButton = () => {
+    this.setState({
+      clearButtonDisabled: this.sigPad.isEmpty()
+    });
+  };
+
+  render() {
+    return (
+      <div>
+        <div className="sig-container">
+          <SignatureCanvas
+            penColor="black"
+            canvasProps={{ width: 500, height: 200, className: 'sig-canvas' }}
+            onEnd={this.updateClearButton}
+            ref={ref => {
+              this.sigPad = ref;
+            }}
+          />
+          <button onClick={this.clear} disabled={this.state.clearButtonDisabled}>
+            Clear
+          </button>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Signature;
